f0ee1edfa68c097f27a47eeb248083c1bb238f29
KP383 <79580721+KP383@users.noreply.github.com>
2021-06-05 12:42:45 +0530

    Add files via upload

---
+++ Note2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note2() {
+  return (
+    <div className="note">
+      <h1>What I learn from this bootcamp?</h1>
+      <p>
+        In this bootcamp we learn very basic things about HTML and Javascript
+        which very useful for any biginer in web devlopment.
+      </p>
+    </div>
+  );
+}
+
+export default Note2;
---
+++ Note3.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note3() {
+  return (
+    <div className="note">
+      <h1>Feedback</h1>
+      <p>
+        After this bootcamp I fill my intrest in web devlopment and i fill that
+        i become a good full stake devloper in future. I was enjoy that bootcamp
+        from learning point of view.
+      </p>
+    </div>
+  );
+}
+
+export default Note3;
